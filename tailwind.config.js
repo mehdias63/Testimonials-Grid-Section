bd6af5fb301f9d0ca2f14520b71bbf5cbb8388d6
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-01-18 13:17:01 +0330

    last update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       barl: ["Barlow Semi Condensed"],
@@ -11,5 +8,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
-
+};
